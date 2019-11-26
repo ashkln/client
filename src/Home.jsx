eb2c2f2eec902be78e763f05a0537875a3a50c53
Ash Klempton <ashutoshnayakash@gmail.com>
2019-11-26 18:31:30 +0530

    added changes to monitor and home

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,7 @@ export class Home extends Component {
                 <div className="center-container">
                     <div className="header">
                         <h1>A Simple Plant Monitor</h1>
+                        <div className="border-bottom"></div>
                     </div>
                     <div className="description">
                         <p>This Monitoring application takes temperature and humidity readings from locally fitted sensors and displays them in a real time environment.</p>
@@ -15,7 +16,7 @@ export class Home extends Component {
                     </div>
                     <div className="start-button">
                         <Link to="/monitor">
-                        <button className="start-monitor">Start Monitor</button>
+                            <button className="start-monitor">Start Monitor</button>
                         </Link>
                     </div>
                 </div>
--- src/Monitor.jsx
+++ src/Monitor.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import { Scatter } from 'react-chartjs-2';
+import Loader from 'react-loader-spinner';
 
 export class Monitor extends Component {
     constructor(props) {
@@ -45,11 +46,11 @@ export class Monitor extends Component {
         let humidDataLength = this.state.humidityArray.length
         let tempDataLength = this.state.temperatureArray.length
 
-        if (humidDataLength > 10) {
+        if (humidDataLength > 9) {
             this.state.humidityArray.shift();
         }
 
-        if (tempDataLength > 10) {
+        if (tempDataLength > 9) {
             this.state.temperatureArray.shift();
         }
     }
@@ -67,51 +68,59 @@ export class Monitor extends Component {
 
 
     render() {
-        return (
-            <div>
-                <div className="flex-container">
-                    <div className="chart-container">
-                        <Scatter
-                            data={{
-                                datasets: [
-                                    {
-                                        label: "Temperature",
-                                        backgroundColor: 'rgba(75,192,192,1)',
-                                        borderColor: 'rgb(100, 164, 201)',
-                                        borderWidth: 3,
-                                        fill: false,
-                                        data: this.state.temperatureArray,
-                                        showLine: true
-                                    },
-                                    {
-                                        label: "Humidity",
-                                        backgroundColor: 'rgb(50, 166, 90)',
-                                        borderColor: 'rgb(50, 166, 90)',
-                                        borderWidth: 3,
-                                        fill: false,
-                                        data: this.state.humidityArray,
-                                        showLine: true
-                                    }
-                                ]
-                            }}
-                            options={{
-                                scales:{
-                                    xAxes:[{
-                                        type:'time',
-                                        ticks:{
-                                            autoSkip:true,
-                                            maxTicksLimit:11,
-                                            fontSize:8
+
+        if (this.state.humidityArray.length > 1 && this.state.temperatureArray.length > 1) {
+            return (
+                <div>
+                    <div className="flex-container">
+                        <div className="chart-container">
+                            <Scatter
+                                data={{
+                                    datasets: [
+                                        {
+                                            label: "Temperature",
+                                            backgroundColor: 'rgba(75,192,192,1)',
+                                            borderColor: 'rgb(100, 164, 201)',
+                                            borderWidth: 3,
+                                            fill: false,
+                                            data: this.state.temperatureArray,
+                                            showLine: true
+                                        },
+                                        {
+                                            label: "Humidity",
+                                            backgroundColor: 'rgb(50, 166, 90)',
+                                            borderColor: 'rgb(50, 166, 90)',
+                                            borderWidth: 3,
+                                            fill: false,
+                                            data: this.state.humidityArray,
+                                            showLine: true
                                         }
-                                    }]
-                                }
-                            }}
-                        />
+                                    ]
+                                }}
+                                options={{
+                                    scales: {
+                                        xAxes: [{
+                                            type: 'time',
+                                            ticks: {
+                                                maxTicksLimit: 11,
+                                                fontSize: 8
+                                            }
+                                        }]
+                                    }
+                                }}
+                            />
+                        </div>
                     </div>
                 </div>
+            )
+        }
+        return (
+            <div className="loader">
+                <Loader type="ThreeDots" color="#00000" height={80} width={80}/>
             </div>
         )
     }
+
 }
 
 export default Monitor
